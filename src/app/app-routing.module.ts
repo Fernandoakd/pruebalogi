a05a1e4b59a44c85bebb1a796e6b70bd50b8d207
Fer Valdez <fernandoakdvaldez@gmail.com>
2021-08-23 19:23:50 -0300

    prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VehiculoComponent } from './vehiculo/vehiculo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'vehiculo', component: VehiculoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
